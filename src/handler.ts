4c8246ff93744d2ded761f538326014b543401ff
Maxwel <maxwelfcoelho@gmail.com>
2022-03-15 08:18:14 -0300

    add eslint

--- src/handler.ts
+++ src/handler.ts
@@ -5,32 +5,32 @@ import { getRandomQuote } from './services/quote.service';
 
 export const getRandomQuoteHandler: Handler = async (event: any) => {
   
-  try {
-  const randomQuote = await getRandomQuote();
+    try {
+        const randomQuote = await getRandomQuote();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: randomQuote,
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    message: randomQuote,
+                    input: event,
+                },
+                null,
+                2
+            ),
+        };
 
-  } catch(err: any) {
-    return {
-      statusCode: 424,
-      body: JSON.stringify(
-        {
-          message: err.message,
-          input: event,
-        },
-        null,
-        2
-      )
+    } catch(err: any) {
+        return {
+            statusCode: 424,
+            body: JSON.stringify(
+                {
+                    message: err.message,
+                    input: event,
+                },
+                null,
+                2
+            )
+        };
     }
-  }
 };
