3bb0d917d19c9fea95f59fdaea71b8ac3d6091d9
Maxwel <maxwelfcoelho@gmail.com>
2022-03-18 10:27:16 -0300

    create the response in adifferent file

--- src/handler.ts
+++ src/handler.ts
@@ -2,35 +2,21 @@ import 'dotenv/config';
 import { Handler } from 'aws-lambda';
 
 import { getRandomQuoteWorker } from './worker';
+import { buildResponse } from './services/buildResponse/build-response.service';
 
 export const getRandomQuoteHandler: Handler = async (event: any) => {
   
     try {
         const randomQuote = await getRandomQuoteWorker();
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(
-                {
-                    message: randomQuote,
-                    input: event,
-                },
-                null,
-                2
-            ),
-        };
-
+        return buildResponse(200, {
+            message: randomQuote,
+            input: event
+        });
     } catch(err: any) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify(
-                {
-                    message: err.message,
-                    input: event,
-                },
-                null,
-                2
-            )
-        };
+        return buildResponse(500, {
+            message: err.message,
+            input: event
+        });
     }
 };
---
+++ src/services/buildResponse/interfaces/body.ts
@@ -0,0 +1,4 @@
+export type ResponseBody = {
+    message: any;
+    input: any;
+}
